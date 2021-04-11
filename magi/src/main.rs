bf38cf60690f1a92629f4e714118a581335cd1ce
Tomasz Rewak <tomaszrewak@gmail.com>
2021-04-11 23:17:27 +0200

    Spawning a notepad.

--- magi/src/main.rs
+++ magi/src/main.rs
@@ -1,3 +1,9 @@
+use std::process::{Command};
+
 fn main() {
+    Command::new(r"C:\Windows\System32\notepad.exe")
+        .spawn()
+        .expect("Failed to execute command");
+
     println!("Hello, world 1!");
 }
