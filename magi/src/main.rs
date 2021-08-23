38ea0678f8605ac9493ab9976d037208b1851561
Tomasz Rewak <tomaszrewak@gmail.com>
2021-08-23 21:57:23 +0200

    Building protos.

--- magi/src/main.rs
+++ magi/src/main.rs
@@ -1,9 +1,18 @@
 mod application;
 mod instance;
+mod proto;
+
+use proto::{StartApplicationReply, StartApplicationRequest};
 
 use application::Application;
 
 fn main() {
+    let a = StartApplicationReply{
+        error: Option::Some("gfgfgf".to_string())
+    };
+
+    println!("{:?}", a.error);
+
     let mut application = Application::new(
         "test".to_string(),
         r"C:\Windows\System32\notepad.exe".to_string(),
