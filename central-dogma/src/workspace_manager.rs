394a8b16387af8dc202a965a15669f9e6f97b575
Tomasz Rewak <tomaszrewak@gmail.com>
2021-09-04 21:40:24 +0200

    Bidirectional stream

--- central-dogma/src/workspace_manager.rs
+++ central-dogma/src/workspace_manager.rs
@@ -1,41 +1,24 @@
 use super::proto;
-use tonic::{Request, Response, Status};
+use futures_core::Stream;
+use std::pin::Pin;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Request, Response, Status, Streaming};
 
 #[derive(Debug, Default)]
 pub struct WorkspaceManager {}
 
 #[tonic::async_trait]
 impl proto::workspace_manager_server::WorkspaceManager for WorkspaceManager {
-    async fn login(
+    type ListenStream = Pin<
+        Box<
+            dyn Stream<Item = Result<proto::ServerToClientMessage, Status>> + Send + Sync + 'static,
+        >,
+    >;
+
+    async fn listen(
         &self,
-        reqest: Request<proto::LoginRequest>,
-    ) -> Result<Response<proto::LoginReply>, Status> {
-        let reply = proto::LoginReply {
-            workstation_id: Option::Some(1),
-        };
-
-        Ok(Response::new(reply))
-    }
-
-    async fn start_application(
-        &self,
-        request: Request<proto::StartApplicationRequest>,
-    ) -> Result<Response<proto::StartApplicationReply>, Status> {
-        let reply = proto::StartApplicationReply {
-            error: Option::Some("gfgfgf".to_string()),
-        };
-
-        Ok(Response::new(reply))
-    }
-
-    async fn run_task(
-        &self,
-        request: Request<proto::RunTaskRequest>,
-    ) -> Result<Response<proto::RunTaskReply>, Status> {
-        let reply = proto::RunTaskReply {
-            error: Option::Some("gfgfgf".to_string()),
-        };
-
-        Ok(Response::new(reply))
+        request: Request<Streaming<proto::ClientToServerMessage>>,
+    ) -> Result<Response<Self::ListenStream>, Status> {
+        unimplemented!()
     }
 }
