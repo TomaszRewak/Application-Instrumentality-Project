d63f26c5ecb9adaad98dde3584c984a575143fb7
Tomasz Rewak <tomaszrewak@gmail.com>
2021-09-14 22:55:36 +0200

    Message decoding.

--- magi/src/main.rs
+++ magi/src/main.rs
@@ -3,13 +3,15 @@ mod instance;
 mod proto;
 mod workstation_manager;
 
-use std::io::Read;
+use application::Application;
+use bytes::Buf;
+use bytes::BytesMut;
+use mio::windows::NamedPipe;
 use prost::Message;
+use proto::client_server::workstation_manager_client::WorkstationManagerClient;
+use std::io::Read;
 use tokio::time::sleep;
 use tokio::time::Duration;
-use mio::windows::NamedPipe;
-use application::Application;
-use proto::client_server::workstation_manager_client::WorkstationManagerClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,15 +20,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut named_pipe = NamedPipe::new(r"\\.\pipe\magi-workspace-manager").unwrap();
         println!("Created named pipe");
 
-        let mut buffer = [0u8; 1000];
+        let mut buffer = BytesMut::with_capacity(1024);
+        let mut next_message_length: usize = 0;
+        let mut is_next_message_length_complete = false;
+        let mut number_of_next_message_length_chunks = 0;
 
-        named_pipe.read(&mut buffer);
+        loop {
+            named_pipe.read(&mut buffer);
 
-        //named_pipe.
+            if !is_next_message_length_complete && !buffer.is_empty() {
+                let message_size_chunk = buffer.get_u8() as usize;
 
-        proto::local_management::Request::decode_length_delimited(buffer);
+                next_message_length = next_message_length
+                    | ((message_size_chunk & 0b0111_1111)
+                        << (number_of_next_message_length_chunks * 7));
 
-        sleep(Duration::from_millis(5000)).await;
+                if message_size_chunk & 0b1000_0000 != 0 {
+                    is_next_message_length_complete = true;
+                }
+
+                number_of_next_message_length_chunks += 1;
+            }
+
+            if is_next_message_length_complete && buffer.len() == next_message_length {
+                let mut sub_buffer = buffer.take(next_message_length);
+
+                proto::local_management::Request::decode(&mut sub_buffer);
+
+                buffer = sub_buffer.into_inner();
+            }
+
+
+            sleep(Duration::from_millis(1000)).await;
+        }
     });
 
     let mut client = WorkstationManagerClient::connect("http://[::1]:5122").await?;
