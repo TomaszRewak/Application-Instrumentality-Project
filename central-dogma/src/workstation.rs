b366e26e2e06c8970f2b66d2e35164ba2eee99dc
Tomasz Rewak <tomaszrewak@gmail.com>
2022-01-08 23:05:36 +0100

    Fixing the client-server proto

--- central-dogma/src/workstation.rs
+++ central-dogma/src/workstation.rs
@@ -40,10 +40,10 @@ impl Workstation {
             Some(proto::client_server::client_to_server_message::OneOf::LoginRequest(login_request)) => {
                 self.process_login_request(login_request).await;
             }
-            Some(proto::client_server::client_to_server_message::OneOf::StartApplicationReply(
-                start_application_reply,
+            Some(proto::client_server::client_to_server_message::OneOf::CreateInstanceReply(
+                create_instance_reply,
             )) => {
-                self.process_start_application_reply(start_application_reply);
+                self.process_create_instance_reply(create_instance_reply);
             }
             Some(proto::client_server::client_to_server_message::OneOf::RunTaskReply(run_task_reply)) => {
                 self.process_run_task_reply(run_task_reply);
@@ -55,25 +55,25 @@ impl Workstation {
         }
     }
 
-    async fn process_login_request(&self, login_request: proto::LoginRequest) {
+    async fn process_login_request(&self, login_request: proto::client_server::LoginRequest) {
         println!("Received login request {:?}", login_request.hostname);
 
         self.sender.send(proto::client_server::ServerToClientMessage {
             one_of: Some(proto::client_server::server_to_client_message::OneOf::LoginReply(
-                proto::LoginReply { error: None },
+                proto::client_server::LoginReply { error: None },
             )),
         }).await;
     }
 
-    fn process_start_application_reply(
+    fn process_create_instance_reply(
         &self,
-        start_application_reply: proto::StartApplicationReply,
+        start_application_reply: proto::CreateInstanceReply,
     ) {
     }
 
     fn process_run_task_reply(&self, run_task_reply: proto::RunTaskReply) {}
 
-    fn process_heartbeat(&self, heartbeat: proto::Heartbeat)
+    fn process_heartbeat(&self, heartbeat: proto::client_server::Heartbeat)
     {
         println!("Received heartbeat {:?}", heartbeat);
     }
