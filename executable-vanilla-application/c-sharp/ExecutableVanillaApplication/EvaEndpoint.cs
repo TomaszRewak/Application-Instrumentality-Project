e6dfff9c0f1373d123e5e317670b60514854deac
Tomasz Rewak <tomaszrewak@gmail.com>
2022-01-11 00:21:27 +0100

    Starting the work on C# client.

---
+++ executable-vanilla-application/c-sharp/ExecutableVanillaApplication/EvaEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace ExecutableVanillaApplication
+{
+	public class EvaEndpoint
+	{
+		public void Start(string pipeName)
+		{
+			var pipe = new NamedPipeClientStream(pipeName);
+			pipe.Connect();
+		}
+	}
+}
